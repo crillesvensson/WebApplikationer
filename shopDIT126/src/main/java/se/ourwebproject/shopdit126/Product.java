0143550a5c16a3e6826d17e302b80ab95702673a
Emil Bogren <emil.bogren@fripost.org>
2013-09-09 11:48:47 +0200

    Added new files

---
+++ shopDIT126/src/main/java/se/ourwebproject/shopdit126/Product.java
@@ -0,0 +1,14 @@
+package se.ourwebproject.shopdit126;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emilbogren
+ */
+public class Product {
+    
+}

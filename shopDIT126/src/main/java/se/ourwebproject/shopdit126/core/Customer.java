38843e42aab13cc131347f6832e64a20ad972e8f
Emil Bogren <emil.bogren@fripost.org>
2013-09-09 14:35:50 +0200

    Added skel-customer

--- shopDIT126/src/main/java/se/ourwebproject/shopdit126/core/Customer.java
+++ shopDIT126/src/main/java/se/ourwebproject/shopdit126/core/Customer.java
@@ -12,4 +12,70 @@ package se.ourwebproject.shopdit126.core;
  */
 public class Customer {
     
+    /*
+     * Contact info, billing and shipping adress in adressclass  
+     * 
+     */
+    private final String fname; 
+    private final String lname;
+    private final String email;
+    private final Address billingAddress;
+    private final Address shippingAddress;
+    // cart 
+    // newsletter yes no
+    // phone nr
+    
+    public Customer(Address billingAddress, Address shippingAddress, String fname,
+            String lname, String email){
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.billingAddress = billingAddress;
+        this.shippingAddress = shippingAddress;
+    } 
+    
+    public Customer(Long customerId, Address billingAddress, Address shippingAddress, String fname,
+            String lname, String email){
+        //super(customderId);
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.billingAddress = billingAddress;
+        this.shippingAddress = shippingAddress;
+    } 
+    
+    //Add product to cart 
+    
+    //remove product 
+    
+    //empty cart
+    
+    public Address getBillingAddress(){
+        return billingAddress;
+    }
+    
+    public Address getShippingAddress(){
+        return shippingAddress;
+    }
+    
+    public String getFirstName(){
+        return fname;
+    }
+    
+    public String getLastName(){
+        return lname;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    //Add more return ?
+    
+    
+    @Override
+    public String toString(){
+        return "";
+    }
+    
 }

27c8eb1a15947042324724c25838c6ed32aabfcf
Emil Bogren <emil.bogren@fripost.org>
2013-09-30 14:01:49 +0200

    Readded project frame - Renamed Project

---
+++ Group4Shop/src/main/java/dit126/group4/group4shop/App.java
@@ -0,0 +1,13 @@
+package dit126.group4.group4shop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

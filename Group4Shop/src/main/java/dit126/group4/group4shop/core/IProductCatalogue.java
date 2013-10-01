cdb1d5de1da6ec3a9f5c58a07a2a8c92f4326bd2
crillesvensson <crillesvensson22@gmail.com>
2013-10-01 15:19:13 +0200

    Added methods in interface

--- Group4Shop/src/main/java/dit126/group4/group4shop/core/IProductCatalogue.java
+++ Group4Shop/src/main/java/dit126/group4/group4shop/core/IProductCatalogue.java
@@ -4,10 +4,22 @@
  */
 package dit126.group4.group4shop.core;
 
+import java.util.List;
+
 /**
  *
  * @author Christian
  */
 public interface IProductCatalogue {
-    
+    public void add(Product t);
+
+    public void remove(Long id);
+
+    public void update(Product t);
+
+    public Product find(Long id);
+
+    public List<Product> getRange(int first, int nItems);
+
+    public int getCount();
 }

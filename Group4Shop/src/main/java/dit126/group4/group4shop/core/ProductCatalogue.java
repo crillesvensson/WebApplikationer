bfc41dcdab6de1a5f99cfed8a7c5c76074163290
crillesvensson <crillesvensson22@gmail.com>
2013-10-01 14:58:38 +0200

    Added shop and productcatalogue

---
+++ Group4Shop/src/main/java/dit126/group4/group4shop/core/ProductCatalogue.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dit126.group4.group4shop.core;
+
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Christian
+ */
+public class ProductCatalogue implements IProductCatalogue{
+ 
+    
+    private EntityManagerFactory emf;
+    //private final Class<Product> clazz;
+    
+    
+}

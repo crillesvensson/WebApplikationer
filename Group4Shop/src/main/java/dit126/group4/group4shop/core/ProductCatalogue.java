6c2f9b4df1e066a73913a8cdb76c61935e2d2236
crillesvensson <crillesvensson22@gmail.com>
2013-10-01 15:17:01 +0200

    Add, update, remove, find, getRange, count

--- Group4Shop/src/main/java/dit126/group4/group4shop/core/ProductCatalogue.java
+++ Group4Shop/src/main/java/dit126/group4/group4shop/core/ProductCatalogue.java
@@ -4,8 +4,11 @@
  */
 package dit126.group4.group4shop.core;
 
+import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -20,6 +23,83 @@ public class ProductCatalogue implements IProductCatalogue{
     public ProductCatalogue(String puName){
         this.emf = Persistence.createEntityManagerFactory(puName);
         this.clazz = Product.class;
+    }
+    
+
+    public void add(Product product) {
+        EntityManager em = this.emf.createEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.persist(product);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+    
+    public void remove(Long id) {
+        EntityManager em = this.emf.createEntityManager();
+        Product product = em.find(clazz, id);
+        try{
+            em.getTransaction().begin();
+            em.remove(product);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public void update(Product product) {
+        EntityManager em = this.emf.createEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.merge(product);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+        }
+    }
+
+    public Product find(Long id) {
+        EntityManager em = this.emf.createEntityManager();
+        Product product = null;
+        try{
+            em.getTransaction().begin();
+            product = em.find(clazz, id);
+            em.getTransaction().commit();
+        }finally{
+            em.close();
+            return product;
+        }
+    }
+
+    public List<Product> getRange(int first, int nItems) {
+        EntityManager em = this.emf.createEntityManager();
+        Query query = null;
+        List<Product> list = null;
+        try{
+            em.getTransaction().begin();
+            query = em.createQuery("SELECT p FROM "+ clazz.getSimpleName().toUpperCase() + " p");
+            em.getTransaction().commit();
+            list = query.getResultList();
+        }finally{
+            em.close();
+            return list.subList(first, nItems);
+        }
+    }
+
+    public int getCount() {
+        EntityManager em = this.emf.createEntityManager();
+        int count = 0;
+        try{
+            em.getTransaction().begin();
+            Query query = em.createQuery("SELECT p FROM " + clazz.getSimpleName().toUpperCase() + " p");
+            em.getTransaction().commit();
+            count = query.getResultList().size();
+        }finally{
+            em.close();
+            return count;
+        }
     }
     
 }

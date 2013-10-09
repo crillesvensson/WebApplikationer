d769b88c13be88aced62840a703bdaa35b526879
oskarssd <gusoskada@student.gu.se>
2013-10-09 18:06:30 +0200

    comment on tests

--- Group4Shop/src/test/java/dit126/group4/group4shop/core/TestDatabase.java
+++ Group4Shop/src/test/java/dit126/group4/group4shop/core/TestDatabase.java
@@ -48,7 +48,7 @@ public class TestDatabase {
             Users user = new Users("emil@test.se", "Emil", "B", "qwerty");
             Roles role = new Roles("Admin", "En admin");
             
-            shop.getUserregister().add(user);
+            shop.getUserRegister().add(user);
             System.out.println("  TEST:  " + shop.getRolesRegister()  );
             shop.getRolesRegister().add(role);
             

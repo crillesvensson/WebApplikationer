39beeb520aaec293533daca5245309dfaf806ac8
oskarssd <gusoskada@student.gu.se>
2013-10-11 00:04:19 +0200

    Learned some CSS

--- Group4Shop_App/src/main/webapp/js/core/shop.js
+++ Group4Shop_App/src/main/webapp/js/core/shop.js
@@ -1,6 +1,6 @@
 var shop = (function(){
     
-    var baseUri = "http://localhost:8080/Group4Shop/";  //change when deciding pattern
+    var baseUri = "http://localhost:8080/Group4Shop_App/";  //change when deciding pattern
     var products = new ProductCatalogue(baseUri + "products");
     
     return {

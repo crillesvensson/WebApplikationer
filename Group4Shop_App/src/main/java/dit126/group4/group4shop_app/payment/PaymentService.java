6d33b7558176d6e934c8df8acf7d82e8ef61355c
Emil Bogren <emil.bogren@fripost.org>
2013-10-18 19:53:22 +0200

    Added simple simulator-payment-service

--- Group4Shop_App/src/main/java/dit126/group4/group4shop_app/payment/PaymentService.java
+++ Group4Shop_App/src/main/java/dit126/group4/group4shop_app/payment/PaymentService.java
@@ -4,10 +4,58 @@
  */
 package dit126.group4.group4shop_app.payment;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author emilbogren
  */
+@Named("paymentservice")
+@RequestScoped
 public class PaymentService {
+    
+    private String cardnumber;
+    private String validUntil;
+    private String cardHolder;
+    private String cvc;
+    
+    
+    public String doPayment(){
+        System.out.println("Payment reached paymentservice with info: " + cardnumber + 
+                " " + validUntil + " " + cardHolder + " " + cvc );
+        if(cardnumber != null && cvc != null){
+            try {
+                int random = 2 + (int)(Math.random() * (10 - 4));
+                System.out.println("Pending payment for: " + random + " seconds!");
+                Thread.sleep(random);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PaymentService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return "SUCCESS";
+        } else{
+            return "FAILED";
+        }
+        
+    }
+
+    public void setCardnumber(String cardnumber) {
+        this.cardnumber = cardnumber;
+    }
+
+    public void setValidUntil(String validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public void setCardHolder(String cardHolder) {
+        this.cardHolder = cardHolder;
+    }
+
+    public void setCvc(String cvc) {
+        this.cvc = cvc;
+    }
+    
     
 }

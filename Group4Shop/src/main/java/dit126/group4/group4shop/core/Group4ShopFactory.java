544c0cb0db76440bec6ba4be593d53cab2a695df
forerik <erikforsberg@dhcp-178119.eduroam.chalmers.se>
2013-10-19 13:36:38 +0200

    mergefix

--- Group4Shop/src/main/java/dit126/group4/group4shop/core/Group4ShopFactory.java
+++ Group4Shop/src/main/java/dit126/group4/group4shop/core/Group4ShopFactory.java
@@ -1,7 +1,5 @@
 package dit126.group4.group4shop.core;
 
-import javax.management.relation.Role;
-
 /**
  *
  * @author Christian
@@ -18,7 +16,6 @@ public class Group4ShopFactory {
         return shop;
     }
     
-<<<<<<< HEAD
    private static void init(Group4Shop shop){
         shop.getProductCatalogue().add(new Product(new Long(5), "Rock", 22.00, "Hard"));
         shop.getProductCatalogue().add(new Product(new Long(10), "Feather", 30.00, "We had.. eh.. have a hen in our backyard that sometimes drops these.. by itself"));
@@ -27,10 +24,8 @@ public class Group4ShopFactory {
         shop.getProductCatalogue().add(new Product(new Long(30), "Bench", 100.00, "The kind you sit on"));
         shop.getUserRegister().add(new Users("crillesvensson22@gmail.com", "Christian", "Svensson", "christiansvensson"));
        // shop.getRatingCatalogue().add(new Rating(new Long(1), new Long(1), new Long(5), 3, "Best ever"));
-        shop.getRatingCatalogue().add(new Rating(new Long(1), new Long(1), new Long(5), 3, "Best ever"));
+       // shop.getRatingCatalogue().add(new Rating(new Long(1), new Long(1), new Long(5), 3, "Best ever"));
         shop.getProductCatalogue().add(new Product(new Long(35), "Air", 1000.00, "Luxury air"));
-=======
-    private static void init(Group4Shop shop){
->>>>>>> 2573e92d058ae39c094ef5c6faf67e7163fc8438
-    }
+
+   }
 }

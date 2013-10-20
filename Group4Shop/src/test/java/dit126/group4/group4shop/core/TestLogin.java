a0026f123929f5bdca5c34130c3902249e44ffd6
Emil Bogren <emil.bogren@fripost.org>
2013-10-20 23:03:14 +0200

    All initial data is in test - test should be fixed, for now populate tables

--- Group4Shop/src/test/java/dit126/group4/group4shop/core/TestLogin.java
+++ Group4Shop/src/test/java/dit126/group4/group4shop/core/TestLogin.java
@@ -19,22 +19,22 @@ public class TestLogin {
     private static final String PU_NAME = "group4_shop";
     
     static IGroup4Shop shop;
-        
+    
     @BeforeClass
-   public static void before(){
+    public static void before(){
         shop = Group4ShopFactory.getShop(PU_NAME);
-   }
-   
-   @Test
-   public void testAddAdminUser(){
-       //shop.getProductCatalogue().add(new Product(new Long(10), "Feather", 30.00, "We had.. eh.. have a hen in our backyard that sometimes drops these.. by itself"));
+    }
+    
+    @Test
+    public void testAddAdminUser(){
+        //shop.getProductCatalogue().add(new Product(new Long(10), "Feather", 30.00, "We had.. eh.. have a hen in our backyard that sometimes drops these.. by itself"));
         //shop.getProductCatalogue().add(new Product(new Long(15), "Water", 42.00, "Freshly harvested from last weeks rain"));
         //shop.getProductCatalogue().add(new Product(new Long(25), "Fire", 82.00, "Order a fire online, we will deliver it to you around midnight when we are sure that you are asleep"));
         //shop.getProductCatalogue().add(new Product(new Long(30), "Bench", 100.00, "The kind you sit on"));
         //shop.getProductCatalogue().add(new Product(new Long(35), "Air", 1000.00, "Luxury air"));
-       
-       
-       
+        
+        
+        
         Product product = new Product(new Long(5), "Rock", 22.00, "Hard");
         Product product2 = new Product(new Long(10), "Feather", 30.00, "We had.. eh.. have a hen in our backyard that sometimes drops these.. by itself");
         Product product3 = new Product(new Long(15), "Water", 42.00, "Freshly harvested from last weeks rain");
@@ -52,19 +52,19 @@ public class TestLogin {
         Rating rating6 = new Rating(new Long(6), user.getEmail(), product6.getId(), 1, "Not good");
         PurchaseOrder purchaseOrder = new PurchaseOrder(new Long(1), user);
         OrderItem orderItem = new OrderItem(new Long(1), product, 2, purchaseOrder.getId());
-        Roles role = new Roles("admin", "admin role");
-        shop.getRolesRegister().add(role);
+        //Roles role = new Roles("admin", "admin role");
+        //shop.getRolesRegister().add(role);
         Roles adminRole = shop.getRolesRegister().get("admin");
         
         UserRoles adminUserRole = new UserRoles(user, adminRole);
-       
+        
         shop.getProductCatalogue().add(product);
         shop.getProductCatalogue().add(product2);
         shop.getProductCatalogue().add(product3);
         shop.getProductCatalogue().add(product4);
         shop.getProductCatalogue().add(product5);
         shop.getProductCatalogue().add(product6);
-              
+        
         shop.getUserRegister().add(user);
         
         shop.getAddressCatalogue().add(address);
@@ -75,14 +75,14 @@ public class TestLogin {
         shop.getRatingCatalogue().add(rating4);
         shop.getRatingCatalogue().add(rating5);
         shop.getRatingCatalogue().add(rating6);
-       
+        
         shop.getOrderBook().add(purchaseOrder);
         
         shop.getOrderItemCatalogue().add(orderItem);
         
         shop.getUserRolesRegister().add(adminUserRole);
-    
+        
         assertTrue(true);
-}
+    }
     
 }

2430d51c1fb3a67510fc0eb56585a9f210466f7d
Emil Bogren <emil.bogren@fripost.org>
2013-10-21 10:44:45 +0200

    Small changes to login, fixed receipt-pdf now render jsf-tags, fix url-pattern

--- Group4Shop_App/src/main/java/dit126/group4/group4shop_app/controller/RenderPDFController.java
+++ Group4Shop_App/src/main/java/dit126/group4/group4shop_app/controller/RenderPDFController.java
@@ -12,7 +12,9 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xhtmlrenderer.pdf.ITextUserAgent;
 
 /**
  *
@@ -27,29 +29,25 @@ public class RenderPDFController {
     public void createPDF(){
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ExternalContext externalContext = facesContext.getExternalContext();
+        HttpSession session = (HttpSession) externalContext.getSession(true);
         
-        
-        String url = "http://localhost:8080/Group4Shop_App/content/checkout/receiptframe.xhtml";  
+        String url = "http://localhost:8080/Group4Shop_App/content/checkout/receiptframe.xhtml"+";jsessionid=" + session.getId();  
         
         try {
             System.out.println("Trying to create receipt");
 
             ITextRenderer renderer = new ITextRenderer();
             renderer.setDocument(new URL(url).toString());
+            //ITextUserAgent callback = new ITextUserAgent(renderer.getOutputDevice());
+            //callback.
             
-            //renderer.layout();
-            //HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
-            //response.reset();
-            //response.setContentType("application/pdf");
-            //response.setHeader("Content-Disposition", "inline; filename=\"" + "hejhopp" + "\"");
-            
-            //OutputStream browserStream = response.getOutputStream();
             OutputStream filepath = new FileOutputStream("/Users/emilbogren/Documents/"
                     + "WebbApp/Project/WebApplikationer/Group4Shop_App/src/main/webapp/"
                     + "content/checkout/receipts/Receipt#"+"orderID"+ ".pdf");
             renderer.layout();
             renderer.createPDF(filepath);
             filepath.close();
+            
             System.out.println("SUCCESS FILE IS CREATED");
             
         } catch (Exception ex) {

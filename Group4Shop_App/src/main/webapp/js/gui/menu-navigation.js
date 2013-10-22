9485dc7934114ae44d12da8be158b2b75efddfc5
Emil Bogren <emil.bogren@fripost.org>
2013-10-22 18:53:50 +0200

    None

--- Group4Shop_App/src/main/webapp/js/gui/menu-navigation.js
+++ Group4Shop_App/src/main/webapp/js/gui/menu-navigation.js
@@ -1,8 +1,6 @@
 $(function(){
     $('#aboutmenu li a').on('click', function(e){
-        e.preventDefault();
-        var page_url=$(this).prop('href');
-        $('#info-text-box').load(page_url);
+        $('#info-text-box').load();
     });
 });
 
